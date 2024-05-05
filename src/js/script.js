7760ae0b303a68ad783ad0cf2191bf9d265073cf
Felipe <menfelipe1@gmail.com>
2024-05-05 19:26:09 -0300

    Adicionando função para mudar a cor do fundo

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,27 @@ function slideShow(){
         setTimeout('slideShow()',time);
 }
 slideShow();
+
+function validarForm(){
+  let nome = document.getElementById("#nome");
+  let email = document.getElementById("#email");
+  let telefone = document.getElementById("#telefone");
+
+  if(nome == "" || email == "" || telefone == ""){
+    alert("Todos os espaços devem estar preenchidos!");
+    
+  }
+}
+
+function mudarCor(){
+  function mudar(numero){
+      return(Math.random()* numero);
+  }
+  const cores= `rgb(${mudar(255)}, ${mudar(255)}, ${mudar(255)})`;
+  document.body.style.backgroundColor=cores;
+}
+  window.addEventListener('load',function(){
+  this.setInterval(function(){
+      mudarCor();
+  },3000)
+  })
